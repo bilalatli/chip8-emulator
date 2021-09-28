40140f4a6b80feb3b5aaed182ef256e106d82111
Bilal ATLI <bilal@socialtradex.com>
2021-09-28 16:48:22 +0300

    Add Memory Module

--- src/hardware/memory.ts
+++ src/hardware/memory.ts
@@ -4,4 +4,16 @@ export class Memory {
     constructor() {
         this.memory = new Uint8Array(4096);
     }
+
+    public get(memoryAddress: number): number {
+        return this.memory[memoryAddress];
+    }
+
+    public set(memoryAddress: number, data: number): void {
+        this.memory[memoryAddress] = data;
+    }
+
+    public reset() {
+        this.memory = new Uint8Array(4096);
+    }
 }
