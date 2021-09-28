513cbac8943837169b22afdf304c6d88e7528d2e
Bilal ATLI <bilal@socialtradex.com>
2021-09-28 17:15:46 +0300

    Add Comments on Memory Module

--- src/hardware/memory.ts
+++ src/hardware/memory.ts
@@ -1,21 +1,37 @@
-import {DeviceConstants} from "@app/config/device-constants";
-
 export class Memory {
+    private readonly memorySize: number;
     private memory: Uint8Array;
 
-    constructor() {
+    /**
+     * Memory module constructor
+     * @param memorySize
+     */
+    constructor(memorySize: number) {
+        this.memorySize = memorySize;
         this.reset();
     }
 
+    /**
+     * Get data from given memory address
+     * @param memoryAddress
+     */
     public get(memoryAddress: number): number {
         return this.memory[memoryAddress];
     }
 
+    /**
+     * Set data to given memory address
+     * @param memoryAddress
+     * @param data
+     */
     public set(memoryAddress: number, data: number): void {
         this.memory[memoryAddress] = data;
     }
 
+    /**
+     * Reset memory
+     */
     public reset() {
-        this.memory = new Uint8Array(DeviceConstants.MEMORY_SIZE);
+        this.memory = new Uint8Array(this.memorySize);
     }
 }
